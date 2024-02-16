47d08b8152e7428f53ad62bbf77ddc3c85a89d0b
valmir473355 <valmir473355@gmail.com>
2024-02-16 04:17:22 -0300

    formatando valores em rais/dolar

--- scripts.js
+++ scripts.js
@@ -6,11 +6,16 @@ const convertValues = () => {
     const inputReais = document.getElementById('input-real').value
     const realValueText = document.getElementById('real-value-text')
     const currencyValueText = document.getElementById('currency-value-text')
+  
 
-    realValueText.innerHTML = inputReais
-    currencyValueText.innerHTML = inputReais / dolar
+    realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
+        { style: 'currency', currency: 'BRL' }
+    ).format(inputReais)
+    
 
-    console.log(inputReais / dolar)
+    currencyValueText.innerHTML = new Intl.NumberFormat('en-US',
+        { style: 'currency', currency: 'USD' }
+    ).format(inputReais / dolar)
 }
 
 
